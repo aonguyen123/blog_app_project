8217bc36fee729949cf893d4493fdfe3a5ab74bb
unknown <aonguyen113@gmail.com>
2020-02-15 19:56:14 +0700

    edit base url

--- src/constants/base_url.js
+++ src/constants/base_url.js
@@ -1,3 +1,4 @@
-export const BASE_URI = 'http://localhost:4000/api/v1';
+export const BASE_URI = 'https://be-pj2.herokuapp.com/api/v1';
 
 //http://localhost:4000/api/v1
+//https://be-pj2.herokuapp.com/api/v1
